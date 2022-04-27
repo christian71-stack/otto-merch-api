4641d93a621b5e0b4d035ea41107e43bcbd65f11
Christian Bohl <c.bohl@cbdesk.de>
2022-04-27 09:32:56 +0200

    Rename Namespace

--- lib/Model/AmountOrdersV4.php
+++ lib/Model/AmountOrdersV4.php
@@ -5,7 +5,7 @@
  * PHP version 5
  *
  * @category Class
- * @package  Swagger\Client
+ * @package  cbdesk\otto\Client
  * @author   Swagger Codegen team
  * @link     https://github.com/swagger-api/swagger-codegen
  */
@@ -26,17 +26,17 @@
  * Do not edit the class manually.
  */
 
-namespace Swagger\Client\Model;
+namespace cbdesk\otto\Client\Model;
 
 use \ArrayAccess;
-use \Swagger\Client\ObjectSerializer;
+use \cbdesk\otto\Client\ObjectSerializer;
 
 /**
  * AmountOrdersV4 Class Doc Comment
  *
  * @category Class
  * @description Amount
- * @package  Swagger\Client
+ * @package  cbdesk\otto\Client
  * @author   Swagger Codegen team
  * @link     https://github.com/swagger-api/swagger-codegen
  */
